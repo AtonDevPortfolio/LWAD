e91795f427a0468261fc775b3c7293325ab14cf8
anton-b117 <anton@maroonspear.com>
2015-05-07 02:58:08 -0400

    refactored.

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var main = function () {
 	"use strict";
 
 	$(".comment-input button").on("click", function (event) {
-		$(".comments").append("<p>This is a new comment.</p>");
+		var $new_comment = $("<p>");
+
+		$new_comment.text("this is a new comment");
+		$(".comments").append($new_comment);
 	});
 };
 
