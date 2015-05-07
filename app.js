efb6e8153e8825c8a1f1e50dabe68adeeb12668f
anton-b117 <anton@maroonspear.com>
2015-05-07 03:18:08 -0400

    Major jQeury bug, wil address later today.

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var main = function () {
 	"use strict";
 
 	$(".comment-input button").on("click", function (event) {
-		var $new_comment = $("<p>");
+		var $new_comment = $("<p>"),
+			comment_text = $(".comment-input input").val();
 
-		$new_comment.text("this is a new comment");
+// single line version, study later
+//var $new_comment = $("<p>").text($(".omment-input input").val);
+
+		$new_comment.text("comment_text");
+
 		$(".comments").append($new_comment);
 	});
 };
