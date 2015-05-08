4661455f821345060c5961fca6774c75802f1de9
anton-b117 <anton@maroonspear.com>
2015-05-08 09:02:27 -0400

    Added keyCode 13 comment submission.

--- app.js
+++ app.js
@@ -9,9 +9,24 @@ var main = function () {
 			$(".comments").append($new_comment);
 			$(".comment-input input").val("");
 		}
-		
+
 	})
-;};
+
+	$(".comment-input input").on("keypress", function (event) {
+		var $new_comment;
+
+		if (event.keyCode === 13) {
+			//if ($(".comment-input input"))
+			//	console.log("Hello, Mordor.  This is keycode " + event.keyCode);
+			if ($(".comment-input input").val() !== "") {
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$(".comments").append($new_comment);
+			$(".comment-input input").val("");
+			}	
+		}
+			
+	});
+};
 
 $(document).ready(main);
 
