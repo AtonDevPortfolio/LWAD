7d3f577365351234219872ab6d9df1def42becda
anton-b117 <anton@maroonspear.com>
2015-05-10 10:05:06 -0400

    Refactered jQuery using DRY.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var main = function () {
 	"use strict";
 
-	$(".comment-input button").on("click", function (event) {
+	var addCommentFromInputBox = function () {
 		var $new_comment;
 
 		if ($(".comment-input input").val() !== "") {
@@ -14,24 +14,25 @@ var main = function () {
 			$(".comment-input input").val("");
 		}
 
-	})
+	};
+
+	$(".comment-input button").on("click", function (event) {
+		addCommentFromInputBox();
+	});
 
 	$(".comment-input input").on("keypress", function (event) {
-		var $new_comment;
-
 		if (event.keyCode === 13) {
-			//if ($(".comment-input input"))
-			//	console.log("Hello, Mordor.  This is keycode " + event.keyCode);
-			if ($(".comment-input input").val() !== "") {
-			$new_comment = $("<p>").text($(".comment-input input").val());
-			$new_comment.hide();
-			$(".comments").append($new_comment);
-			$new_comment.fadeIn();
-			$(".comment-input input").val("");
-			}	
-
+			addCommentFromInputBox();
 		}
-			
+		//if ($(".comment-input input"))
+		//	console.log("Hello, Mordor.  This is keycode " + event.keyCode);
+		// if ($(".comment-input input").val() !== "") {
+		// $new_comment = $("<p>").text($(".comment-input input").val());
+		// $new_comment.hide();
+		// $(".comments").append($new_comment);
+		// $new_comment.fadeIn();
+		// $(".comment-input input").val("");
+		// }	
 	});
 };
 
